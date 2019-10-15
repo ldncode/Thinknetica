4d12cf0590d37e992c8e8f460e80efb9e60724a8
LDN <ledenevdn@gmail.com>
2019-10-15 16:59:07 +0700

    Add ideal_weight.rb

---
+++ Lesson_1/ideal_weight.rb
@@ -0,0 +1,11 @@
+print 'Please, enter your name: '
+name = gets.chomp.capitalize
+
+print 'Please, enter your height: '
+altitude = gets.chomp.to_i
+
+if (altitude - 110) >= 0
+  print "Your fighting weight, #{name} : #{altitude - 110}"
+else
+  print 'Your weight optimal'
+end
