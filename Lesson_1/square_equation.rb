f280ded56add1d4a05886b15974f94f90ef25df1
LDN <ledenevdn@gmail.com>
2019-10-18 19:06:13 +0700

    Add new files in lesson

---
+++ Lesson_1/square_equation.rb
@@ -0,0 +1,21 @@
+print 'Введите коэффециент a: '
+a = gets.chomp.to_f
+
+print 'Введите коэффециент b: '
+b = gets.chomp.to_f
+
+print 'Введите коэффециент c: '
+c = gets.chomp.to_f
+
+D = b**2 - 4 * a * c
+
+if D == 0
+  x1 = (-b) / (2 * a)
+  puts "Дискриминант: #{D}, корень равен #{x1}"
+elsif D > 0
+  x1 = (-b + Math.sqrt(D)) / (2 * a)
+  x2 = (-b - Math.sqrt(D)) / (2 * a)
+  puts "Дискриминант: #{D}, корень x1 равен #{x1}, корень x2 равен #{x2}"
+else
+  puts "Дискриминант: #{D}, корней нет"
+end
