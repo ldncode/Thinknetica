7032a4f4ab8e86e1c2b4f1d5347049a749c0bc49
LDN <ledenevdn@gmail.com>
2019-10-21 16:11:16 +0700

    Fixed  square equation

--- Lesson_1/square_equation.rb
+++ Lesson_1/square_equation.rb
@@ -9,12 +9,14 @@ c = gets.chomp.to_f
 
 D = b**2 - 4 * a * c
 
+
 if D == 0
   x1 = (-b) / (2 * a)
   puts "Дискриминант: #{D}, корень равен #{x1}"
 elsif D > 0
-  x1 = (-b + Math.sqrt(D)) / (2 * a)
-  x2 = (-b - Math.sqrt(D)) / (2 * a)
+  C = Math.sqrt(D)
+  x1 = (-b + C / (2 * a)
+  x2 = (-b - C / (2 * a)
   puts "Дискриминант: #{D}, корень x1 равен #{x1}, корень x2 равен #{x2}"
 else
   puts "Дискриминант: #{D}, корней нет"
