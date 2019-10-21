9f35edb465260d658ac39e682f0a2be0b4b8bef5
LDN <ledenevdn@gmail.com>
2019-10-21 15:56:17 +0700

    Update task

--- Lesson_1/area_figure.rb
+++ Lesson_1/area_figure.rb
@@ -4,5 +4,5 @@ a = gets.chomp.to_f
 print 'Enter the height: '
 h = gets.chomp.to_f
 
-print "The area of the triangle: #{0.5 * a * h}"
+p "The area of the triangle: #{0.5 * a * h}"
 
--- Lesson_1/ideal_weight.rb
+++ Lesson_1/ideal_weight.rb
@@ -5,7 +5,7 @@ print 'Please, enter your height: '
 altitude = gets.chomp.to_i
 
 if (altitude - 110) >= 0
-  print "Your fighting weight, #{name} : #{altitude - 110}"
+  print"Your fighting weight, #{name} : #{altitude - 110}"n
 else
   print 'Your weight optimal'
 end
