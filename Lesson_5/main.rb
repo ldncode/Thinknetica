ac151c262f99206c1d6296520dd57b0d214a205a
LDN <ledenevdn@gmail.com>
2019-11-14 22:27:08 +0700

    Fixed lesson5

--- Lesson_5/main.rb
+++ Lesson_5/main.rb
@@ -1,11 +1,12 @@
 require_relative 'station'
 require_relative 'route'
 require_relative 'train'
-require_relative 'cargo_carriages'
+require_relative 'carriage'
+require_relative 'cargo_carriage'
 require_relative 'cargo_train'
-require_relative 'passenger_carriages'
+require_relative 'passenger_carriage'
 require_relative 'passenger_train'
-require_relative 'carriages'
+
 require_relative 'company'
 require_relative 'instance_counter'
 
